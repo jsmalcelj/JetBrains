22ff35a3880966a24833a5802c33beb3ce614073
jsmalcelj <31723031+jsmalcelj@users.noreply.github.com>
2018-03-23 10:47:31 +0100

    Add files via upload

---
+++ druga vježba/externa.js
@@ -0,0 +1,14 @@
+function iza(){
+    var a = prompt('Sto zelis upisat','Youre mum gay');
+    document.getElementById('id').innerText = a;
+}
+
+console.log(viktor)
+
+function tjp() {
+    let br1=parseInt(prompt('unesi prvi broj'));
+    let br2=parseInt(prompt('unesi drugi broj'));
+    var ops=(br1+br2)*2;
+    var pov=br1*br2;
+    alert('povrsina je $(pov) a opseg $(ops)');
+}
